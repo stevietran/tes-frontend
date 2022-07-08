dfaf63381428a6b8b72cfa7eb14becc714968e16
vutran <tes.softwaree@gmail.com>
2022-07-08 14:46:56 +0800

    tested

--- src/app/components/report-dashboard/report-dashboard.component.ts
+++ src/app/components/report-dashboard/report-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CaseService } from 'src/app/case.service';
 import { DashboardItem} from 'src/app/models/case';
+import { M_DASHBOARD } from 'src/assets/m-data/m-dashboard';
 
 @Component({
   selector: 'app-report-dashboard',
@@ -11,15 +12,18 @@ import { DashboardItem} from 'src/app/models/case';
 export class ReportDashboardComponent implements OnInit {
   //DbItems: DashboardItem[] = M_DASHBOARD
   DbItems: DashboardItem[] = []
-  
+  DbItems_done: DashboardItem[] = []
+  DbItems_submitted: DashboardItem[] = []
+  panelOpenState = false;
   constructor(
     private caseService: CaseService,
     private router: Router
     ) { }
 
   ngOnInit(): void {
-    this.getCasesStatus();
   }
+
+
   onNewCase(){
     this.router.navigateByUrl('/design')
   }
@@ -29,5 +33,31 @@ export class ReportDashboardComponent implements OnInit {
       next: (cases) => this.DbItems = cases.result 
     })
 
+    this.DbItems_done = [];
+    this.DbItems_submitted = [];
+
+    for (var item of this.DbItems){
+      if (item.status == "Submitted"){
+        this.DbItems_submitted.push(item);
+      }
+      else{
+        this.DbItems_done.push(item);
+      }
+    }
+  }
+
+  getCasesStatus_mock(): void{
+    this.DbItems = M_DASHBOARD;
+    this.DbItems_done = [];
+    this.DbItems_submitted = [];
+
+    for (var item of this.DbItems){
+      if (item.status == "Submitted"){
+        this.DbItems_submitted.push(item);
+      }
+      else{
+        this.DbItems_done.push(item);
+      }
+    }
   }
 }
--- src/assets/m-data/m-dashboard.ts
+++ src/assets/m-data/m-dashboard.ts
@@ -2,6 +2,11 @@ import { DashboardItem } from "src/app/models/case";
 
 export const M_DASHBOARD: DashboardItem[] = [
     { id: 1, submit_time: "10/11/2021", status: "Finished"},
-    { id: 2, submit_time: "23/02/2022", status: "Submitted"},
-    { id: 3, submit_time: "23/03/2022", status: "Submitted"}
+    { id: 2, submit_time: "02/02/2022", status: "Submitted"},
+    { id: 3, submit_time: "02/03/2022", status: "Submitted"},
+    { id: 6, submit_time: "11/11/2021", status: "Finished"},
+    { id: 7, submit_time: "06/02/2022", status: "Submitted"},
+    { id: 8, submit_time: "05/03/2022", status: "Submitted"},
+    { id: 9, submit_time: "06/02/2022", status: "Submitted"},
+    { id: 10, submit_time: "05/03/2022", status: "Submitted"}
 ]
